b2cb47bd940f0b83b49a34bed73d630dca3393c5
takamasa3288 <takamasa3288@icloud.com>
2021-02-05 07:45:25 +0900

    バリデーションの設定

--- Gemfile
+++ Gemfile
@@ -59,3 +59,4 @@ end
 
 gem 'devise'
 gem 'pry-rails'
+gem 'rspec-rails', '~> 4.0.0'
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :name, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥]+\z/ }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥]+\z/ }
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :birth_day, presence: true
 
   has_many :items
