b08ae7bdbbd8fb4dae6d6399436482bcf710bab4
takamasa3288 <takamasa3288@icloud.com>
2021-02-11 20:53:03 +0900

    ユーザー管理機能（修正）

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,17 +2,21 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-
-  validates :name, presence: true
-  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥]+\z/ }
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥]+\z/ }
-  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :birth_day, presence: true
-
-  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
-
-  validates :password, presence: true, length: { minimum: 6 }, format: { with: VALID_PASSWORD_REGEX }
+  :recoverable, :rememberable, :validatable
+  
+  
+  
+  with_options presence: true do
+    validates :name
+    validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥]+\z/ }
+    validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥]+\z/ }
+    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :birth_day
+  
+    VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
+  
+    validates :password, length: { minimum: 6 }, format: { with: VALID_PASSWORD_REGEX }
+  end
   has_many :items
 end
