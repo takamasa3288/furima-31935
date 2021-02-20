c004ff60421175f2063ef962000c7b7961551ea5
takamasa3288 <takamasa3288@icloud.com>
2021-02-20 14:59:23 +0900

    Revert "itemsモデル単体のテストコード"
    This reverts commit 670daba103a447bd33bb56ee0e76ffbefe009d7b.

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,14 +1,4 @@
 FactoryBot.define do
   factory :item do
-    image {Faker::Lorem.sentence}
-    name { 'test' }
-    description { 'test' }
-    category_id {1}
-    state_id {1}
-    carriage_id {1}
-    area_id {1}
-    days_id {1}
-    price {5000}
-    association :user
   end
 end
