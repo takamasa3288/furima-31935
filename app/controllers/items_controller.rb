d4b81f57cde6d2e766ec9ee088890255f0deb74e
takamasa3288 <takamasa3288@icloud.com>
2021-02-21 11:47:04 +0900

    Revert "Revert "Merge pull request #3 from takamasa3288/itemモデルの作成""
    This reverts commit 28c2697aa9eee2f6b550c99862741ac811733da3.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,26 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.all
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    # binding.pry
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :category_id, :state_id, :carriage_id, :area_id, :days_id, :price,
+                                 :image).merge(user_id: current_user.id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root "items#index"  
+  root to: "items#index"  
+  resources :items, only: [:new, :create]
+  # post "items/new" => "items#new"
 end
