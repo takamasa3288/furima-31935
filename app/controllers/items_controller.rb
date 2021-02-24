7b856ff38595c284add67b976a4598d462fafcaf
takamasa3288 <takamasa3288@icloud.com>
2021-02-24 18:50:17 +0900

    商品編集・削除機能の削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :edit, :destroy]
+  before_action :authenticate_user!, only: [:new]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -20,26 +20,6 @@ class ItemsController < ApplicationController
 
   def show
     @item = Item.find(params[:id])
-  end
-
-  def edit
-    @item = Item.find(params[:id])
-    redirect_to root_path unless @item.user.id == current_user.id
-  end
-
-  def update
-    @item = Item.find(params[:id])
-    if @item.update(item_params)
-      redirect_to root_path
-    else
-      render :edit
-    end
-  end
-
-  def destroy
-    @item = Item.find(params[:id])
-    @item.destroy
-    redirect_to root_path
   end
 
   private
