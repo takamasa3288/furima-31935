4e7b0bdc27e469e74b655ced295b6307ca33437d
takamasa3288 <takamasa3288@icloud.com>
2021-03-19 10:56:09 +0900

    売却済み商品のビュー表示の変更

--- app/models/item.rb
+++ app/models/item.rb
@@ -14,6 +14,7 @@ class Item < ApplicationRecord
     end
   end
   belongs_to :user
+  has_one :order
   has_one_attached :image
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
