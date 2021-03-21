7add26c226fb3ec87788bd5b83c9c8fe089b5046
takamasa3288 <takamasa3288@icloud.com>
2021-03-21 14:58:02 +0900

    可読性の向上

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
-  before_action :move_to_index, only: [:edit, :update, :destroy]
   before_action :sold_out_move_to_index, only: [:edit, :update, :destroy]
 
   def index
@@ -51,11 +50,7 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
-  def move_to_index
-    redirect_to root_path unless @item.user.id == current_user.id
-  end
-
   def sold_out_move_to_index
-    redirect_to root_path if @item.order.present?
+    redirect_to root_path if @item.order.present? || @item.user.id != current_user.id
   end
 end
