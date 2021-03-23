0327c0b606ca559dc77d87116589eac66b76c341
takamasa3288 <takamasa3288@icloud.com>
2021-03-23 22:30:21 +0900

    rubocopの実行

--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe OrderAddress, type: :model do
   before do
     @user = FactoryBot.create(:user)
     @item = FactoryBot.create(:item)
-    @order_address = FactoryBot.build(:order_address, user_id: @user.id , item_id: @item.id)
+    @order_address = FactoryBot.build(:order_address, user_id: @user.id, item_id: @item.id)
     sleep(0.1)
   end
 
@@ -88,19 +88,18 @@ RSpec.describe OrderAddress, type: :model do
         @order_address.valid?
         expect(@order_address.errors.full_messages).to include("Token can't be blank")
       end
-      
+
       it 'user_idが空では登録できない' do
         @order_address.user_id = nil
         @order_address.valid?
         expect(@order_address.errors.full_messages).to include("User can't be blank")
       end
-      
+
       it 'item_idが空では登録できない' do
         @order_address.item_id = nil
         @order_address.valid?
         expect(@order_address.errors.full_messages).to include("Item can't be blank")
       end
-
     end
   end
 end
